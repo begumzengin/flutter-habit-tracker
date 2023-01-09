6a2521f0b8579f60148b9494e685bb8ad63d4a22
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2023-01-09 18:43:44 +0300

    4:14

--- flutter_habit_tracker/lib/pages/home_page.dart
+++ flutter_habit_tracker/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_habit_tracker/components/habit_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +9,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //bool to control habit completed
+  bool habitCompleted = false;
+
+  //checkbox was tapped
+  void checkboxTapped(bool? value) {
+    setState(() {
+      habitCompleted = value!;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +26,11 @@ class _HomePageState extends State<HomePage> {
       body: ListView(
         children: [
           //habit tiles
+          HabitTile(
+            habitName: "morning run",
+            habitCompleted: habitCompleted,
+            onChanged: (value) => checkboxTapped(value),
+          ),
         ],
       ),
     );

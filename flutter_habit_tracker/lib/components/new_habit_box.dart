124d08436330421e52b59d4c29900d423abec972
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2023-01-11 13:03:33 +0300

    8:35

--- flutter_habit_tracker/lib/components/new_habit_box.dart
+++ flutter_habit_tracker/lib/components/new_habit_box.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_habit_tracker/constants.dart';
 
 class EnterNewHabitBox extends StatelessWidget {
   const EnterNewHabitBox({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog();
+    return AlertDialog(
+      content: TextField(),
+      actions: [
+        MaterialButton(
+          onPressed: () {},
+          child: Text(
+            "save",
+            style: TextStyle(color: Colors.white),
+          ),
+          color: strikemaster,
+        ),
+        MaterialButton(
+          onPressed: () {},
+          child: Text(
+            "cancel",
+            style: TextStyle(color: Colors.white),
+          ),
+          color: strikemaster,
+        )
+      ],
+    );
   }
 }

9c970024c08918053a221f4a891ed9bfd4f12c81
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2023-01-11 18:13:43 +0300

    26:25

--- flutter_habit_tracker/lib/data/habit_database.dart
+++ flutter_habit_tracker/lib/data/habit_database.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_habit_tracker/datetime/date_time.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 //reference our box
@@ -13,10 +14,25 @@ class HabitDatabase {
       ["morning run", false],
       ["read book", false],
     ];
+
+    _myBox.put("START_DATE", todaysDateFormatted());
   }
 
   //load data if it already exists
-  void loadData() {}
+  void loadData() {
+    //if it's a new day, get habit list from database
+    if (_myBox.get(todaysDateFormatted()) == null) {
+      todaysHabitList = _myBox.get("CURRENT_HABIT_LIST");
+      //set all habit completed to false since its a new day
+      for (int i = 0; i < todaysHabitList.length; i++) {
+        todaysHabitList[i][i] = false;
+      }
+    }
+    //if it's not a new day, load todays list
+    else {
+      todaysHabitList = _myBox.get(todaysDateFormatted());
+    }
+  }
 
   //update database
   void updateDatabase() {}

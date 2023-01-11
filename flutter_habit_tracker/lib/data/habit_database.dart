32cde143c41e95f516358ea148a060af3eb9273b
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2023-01-11 17:51:02 +0300

    19:12

---
+++ flutter_habit_tracker/lib/data/habit_database.dart
@@ -0,0 +1,14 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+//reference our box
+final _myBox = Hive.box("habit_database");
+
+class HabitDatabase {
+  List todaysHabitList = [];
+
+  //create initial default data
+
+  //load data if it already exists
+
+  //update database
+}
